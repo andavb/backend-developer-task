6ce739a2d29e6a4fffee8ba185f290dcc5c3ac90
Andrej Avbelj <andrej.webzone@gmail.com>
2022-05-20 09:01:22 +0200

    Migration Users fixed.

--- src/migrations/1652868909756-Users.ts
+++ src/migrations/1652868909756-Users.ts
@@ -1,27 +1,24 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import { User } from '../entities';
 import users from './data/users.json';
 
 export class Users1652868909756 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    let r = await queryRunner.manager
-      .createQueryBuilder()
-      .insert()
-      .into('User')
-      .values(users)
-      .orIgnore(`("username") DO NOTHING`)
-      .execute();
+    users.forEach(async ({ name, username, password }) => {
+      let r;
+      r = await User.create({
+        name,
+        username,
+        password,
+      }).save();
 
-    if (r) {
-      console.log(
-        '%c Migration "Users" succesfull..',
-        'background: white; color: green'
-      );
-    } else {
-      console.log(
-        '%c ERROR: Migration "Users"',
-        'background: #222; color: red'
-      );
-    }
+      if (!r) throw new Error('ERROR: Migration "USER"');
+    });
+
+    console.log(
+      '%c Migration "Users" succesfull..',
+      'background: white; color: green'
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
